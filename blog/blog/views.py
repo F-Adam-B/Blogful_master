35d01dcb23f73653a54ed9e73436527de0aa2969
crystalattice <crystalattice@gmail.com>
2017-08-27 16:50:57 -0500

    Added pagination

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -6,9 +6,31 @@ from . import app
 from .database import session, Entry
 
 
-@app.route("/")  # Root (default) page to display when landing on web site
-def entries():  # Query the database entries of the blog
+PAGINATE_BY = 10    # Number of entries per page
+
+
+@app.route("/") # Root (default) page to display when landing on web site
+@app.route("/page/<int:page>")  # Specific site page
+def entries(page=1):     # Query the database entries of the blog
+    # Zero-indexed page
+    page_index = page - 1
+
+    count = session.query(Entry).count()
+
+    start = page_index * PAGINATE_BY  # Index of first entry on page
+    end = start + PAGINATE_BY  # Index of last entry on page
+    total_pages = (count - 1) // PAGINATE_BY + 1  # Total number of pages
+    has_next = page_index < total_pages - 1  # Does following page exit?
+    has_prev = page_index > 0  # Does previous page exist?
+
     entries = session.query(Entry)
     entries = entries.order_by(Entry.datetime.desc())
-    entries = entries.all()
-    return render_template("entries.html", entries=entries)
+    entries = entries[start:end]
+
+    return render_template("entries.html",
+                           entries=entries,
+                           has_next=has_next,
+                           has_prev=has_prev,
+                           page=page,
+                           total_pages=total_pages
+                           )
