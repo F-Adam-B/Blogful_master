3395691649a65d437b6c5821b56f8265288d71f4
crystalattice <crystalattice@gmail.com>
2017-09-16 16:38:41 -0500

    Corrected paths for testing

--- blog/manage.py
+++ blog/manage.py
@@ -12,6 +12,7 @@ from werkzeug.security import generate_password_hash
 
 
 class DB(object):
+    """Define metadata for database"""
     def __init__(self, metadata):
         self.metadata = metadata
 
@@ -25,13 +26,15 @@ manager.add_command("db", MigrateCommand)
 # The function under the each manager.command decorator becomes an argument to the
 # manager script
 @manager.command
-def run():  # When called, create the flask server IP and port
+def run():
+    """When called, create the flask server IP and port"""
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
 
 @manager.command
-def seed():  # Create dummy entries in database
+def seed():
+    """Create dummy entries in database"""
     content = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
     commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
@@ -49,14 +52,15 @@ def seed():  # Create dummy entries in database
 
 @manager.command
 def adduser():
+    """Put a new user in the database"""
     name = input("Name: ")
     email = input("Email: ")
-    if session.query(User).filter_by(email=email).first():
+    if session.query(User).filter_by(email=email).first():  # Check if user email exists
         print("User with that email address already exists")
         return
 
     password = ""
-    while len(password) < 8 or password != password_2:
+    while len(password) < 8 or password != password_2:  # Confirm password meets requirements
         password = getpass("Password: ")
         password_2 = getpass("Re-enter password: ")
     user = User(name=name, email=email,
