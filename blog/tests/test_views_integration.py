b38de2af918f8ccadc0ac125f582c20a5e2996d6
crystalattice <crystalattice@gmail.com>
2017-09-17 17:07:44 -0500

    Modifed test files' config paths

--- blog/tests/test_views_integration.py
+++ blog/tests/test_views_integration.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 from werkzeug.security import generate_password_hash
 
 # Configure your app to use the testing database
-os.environ["CONFIG_PATH"] = "blog.blog.config.TestingConfig"
+os.environ["CONFIG_PATH"] = "blog.config.TestingConfig"
 
 from blog.blog import app
 from blog.blog.database import Base, engine, session, User, Entry
